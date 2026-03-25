31f2baab8e7d2b0b427755fab506f9121e14e9ab
cristian <hancualincristian@gmail.com>
2026-03-25 12:42:17 +0200

    dashboard

--- react/src/pages/Dashboard.tsx
+++ react/src/pages/Dashboard.tsx
@@ -1,49 +1,61 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 
-function Dashboard() { 
-    const navigate = useNavigate();
+function Dashboard() {
+  const navigate = useNavigate();
 
-    return (
-<div className="bg-gray-900 w-80 h-screen flex flex-col justify-between">
+  return (
+    <div className="flex h-screen">
 
-  <div>
-    <div className="px-4 py-4">
-      <h1 className="text-3xl font-bold text-white">
-        Welcome, {localStorage.getItem("username")}!
-      </h1>
+      <div className="bg-gray-900 w-80 flex flex-col justify-between text-white">
+        <div>
+          <div className="px-4 py-4">
+            <h1 className="text-2xl font-bold">
+              Welcome, {localStorage.getItem("username")}!
+            </h1>
+          </div>
+          <ul className="space-y-2 px-4">
+            <li>
+              <Link to="home" className="flex px-2 py-2 text-m hover:bg-gray-700 rounded">
+              <svg className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /> </svg>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="users" className="flex px-2 py-2 text-m hover:bg-gray-700 rounded">
+              <svg className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24"> 
+              <path fill="currentColor" d="M21.053 20.8c-1.132-.453-1.584-1.698-1.584-1.698s-.51.282-.51-.51s.51.51 1.02-2.548c0 0 1.413-.397 1.13-3.68h-.34s.85-3.51 0-4.7c-.85-1.188-1.188-1.98-3.057-2.547s-1.188-.454-2.547-.396c-1.36.058-2.492.793-2.492 1.19c0 0-.85.056-1.188.396c-.34.34-.906 1.924-.906 2.32s.283 3.06.566 3.625l-.337.114c-.284 3.283 1.13 3.68 1.13 3.68c.51 3.058 1.02 1.756 1.02 2.548s-.51.51-.51.51s-.452 1.245-1.584 1.698c-1.132.452-7.416 2.886-7.927 3.396c-.512.51-.454 2.888-.454 2.888H29.43s.06-2.377-.452-2.888c-.51-.51-6.795-2.944-7.927-3.396zm-12.47-.172c-.1-.18-.148-.31-.148-.31s-.432.24-.432-.432s.432.432.864-2.16c0 0 1.2-.335.96-3.118h-.29s.144-.59.238-1.334a10.01 10.01 0 0 1 .037-.996l.038-.426c-.02-.492-.107-.94-.312-1.226c-.72-1.007-1.008-1.68-2.59-2.16c-1.584-.48-1.01-.384-2.16-.335c-1.152.05-2.112.672-2.112 1.01c0 0-.72.047-1.008.335c-.27.27-.705 1.462-.757 1.885v.28c.048.654.26 2.45.47 2.873l-.286.096c-.24 2.782.96 3.118.96 3.118c.43 2.59.863 1.488.863 2.16s-.432.43-.432.43s-.383 1.058-1.343 1.44l-.232.092v5.234h.575c-.03-1.278.077-2.927.746-3.594c.357-.355 1.524-.94 6.353-2.862zm22.33-9.056c-.04-.378-.127-.715-.292-.946c-.718-1.008-1.007-1.68-2.59-2.16c-1.583-.48-1.007-.384-2.16-.335c-1.15.05-2.11.672-2.11 1.01c0 0-.72.047-1.008.335c-.27.272-.71 1.472-.758 1.89h.033l.08.914c.02.23.022.435.027.644c.09.666.21 1.35.33 1.59l-.286.095c-.24 2.782.96 3.118.96 3.118c.432 2.59.863 1.488.863 2.16s-.43.43-.43.43s-.054.143-.164.34c4.77 1.9 5.927 2.48 6.28 2.833c.67.668.774 2.316.745 3.595h.48V21.78l-.05-.022c-.96-.383-1.344-1.44-1.344-1.44s-.433.24-.433-.43s.433.43.864-2.16c0 0 .804-.23.963-1.84V14.66c0-.018 0-.033-.003-.05h-.29s.216-.89.293-1.862z" /> </svg>
+                Users
+              </Link>
+            </li>
+            <li>
+
+              <Link to="media" className="flex px-2 py-2 text-m hover:bg-gray-700 rounded">
+                              <svg className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                </svg>
+                media Library
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="p-4 text-m">
+          <button
+            onClick={() => {
+              localStorage.removeItem("username");
+              navigate("/");
+            }}
+            className="w-full text-left hover:bg-gray-700 px-2 py-2 rounded">
+            Sign out
+          </button>
+        </div>
+      </div>
+      <div className="flex-1 bg-gray-100 p-6">
+        <Outlet />
+      </div>
     </div>
-
-    <aside className="text-white px-4">
-      <ul className="space-y-2 font-medium">
-        <li>
-          <a className="flex items-center text-xl font-serif px-2 py-1.5 hover:bg-gray-700 rounded">
-            <span className="ms-3">home</span>
-          </a>
-        </li>
-        <li>
-          <a className="flex items-center text-xl font-serif px-2 py-1.5 hover:bg-gray-700 rounded">
-            <span className="ms-3">users</span>
-          </a>
-        </li>
-      </ul>
-    </aside>
-  </div>
-
-  <div className="px-4 py-4">
-    <button
-      className="w-full text-left px-2 py-1.5 hover:bg-gray-700 rounded text-white"
-      onClick={() => {
-        localStorage.removeItem("username");
-         navigate("/");
-      }}
-    >
-      Sign out
-    </button>
-  </div>
-
-</div>
-);
+  );
 }
 
 export default Dashboard;
