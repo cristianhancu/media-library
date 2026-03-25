13458df4b06a3080dfaa8585d97697924d875506
cristian <hancualincristian@gmail.com>
2026-03-25 12:15:04 +0200

    dashboard

--- react/src/pages/Dashboard.tsx
+++ react/src/pages/Dashboard.tsx
@@ -1,16 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-function Dashboard() {
 
-  return (
-    <div>
-    <div className=" flex justify-center items-center py-20">
-      <h1 className="text-3xl font-bold text-white  flex justify-center">Welcome, {localStorage.getItem("username")}!</h1>
+function Dashboard() { 
+    const navigate = useNavigate();
+
+    return (
+<div className="bg-gray-900 w-80 h-screen flex flex-col justify-between">
+
+  <div>
+    <div className="px-4 py-4">
+      <h1 className="text-3xl font-bold text-white">
+        Welcome, {localStorage.getItem("username")}!
+      </h1>
     </div>
-    <div className=" flex justify-center items-center py-20">
-      <h1 className="text-3xl font-bold text-white  flex justify-center">Users: {localStorage.getItem("users")}</h1>
-    </div>
-    </div>
-  );
+
+    <aside className="text-white px-4">
+      <ul className="space-y-2 font-medium">
+        <li>
+          <a className="flex items-center text-xl font-serif px-2 py-1.5 hover:bg-gray-700 rounded">
+            <span className="ms-3">home</span>
+          </a>
+        </li>
+        <li>
+          <a className="flex items-center text-xl font-serif px-2 py-1.5 hover:bg-gray-700 rounded">
+            <span className="ms-3">users</span>
+          </a>
+        </li>
+      </ul>
+    </aside>
+  </div>
+
+  <div className="px-4 py-4">
+    <button
+      className="w-full text-left px-2 py-1.5 hover:bg-gray-700 rounded text-white"
+      onClick={() => {
+        localStorage.removeItem("username");
+         navigate("/");
+      }}
+    >
+      Sign out
+    </button>
+  </div>
+
+</div>
+);
 }
+
 export default Dashboard;
